ed7bfe4ac5488b9de6d65f605f64ca6f1490ed8a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-13 13:20:26 +0900

    feat: first TrussC hello world (rotating box) from Julia!

--- src/JlTrussCTest.jl
+++ src/JlTrussCTest.jl
@@ -1,5 +1,7 @@
 module JlTrussCTest
 
+using Printf
+
 module TrussC
   using CxxWrap
   @wrapmodule(() -> normpath(joinpath(@__DIR__, "..", "lib","libJlTrussC")))
@@ -9,18 +11,27 @@ module TrussC
   end
 end # module TrussC
 
+f32(d::Float64) = Float32(d)
+f32(d::Int64) = Float32(d)
+f32(d::Int32) = Float32(d)
+
 function setup()
   println("setup!")
 end
 
 function draw()
-  TrussC.clear(0.12)
+  TrussC.clear(f32(0.12), f32(1.0))
 
   # Rotating box
+  TrussC.pushMatrix();
   TrussC.noFill();
-  TrussC.translate(TrussC.getWindowWidth() / 2, TrussC.getWindowHeight() / 2);
-  TrussC.rotate(TrussC.getElapsedTimef() * 0.1, TrussC.getElapsedTimef() * 0.15, 0);
-  TrussC.drawBox(200.0);
+  TrussC.translate(f32(TrussC.getWindowWidth() / 2), f32(TrussC.getWindowHeight() / 2));
+  TrussC.rotate(f32(TrussC.getElapsedTimef() * 0.1), f32(TrussC.getElapsedTimef() * 0.15), f32(0));
+  TrussC.drawBox(f32(200.0));
+  TrussC.popMatrix();
+
+  # show fps
+  TrussC.drawBitmapString(@sprintf("fps: %.1f", TrussC.getFps()), f32(30), f32(30));
 end
 
 function keyPressed(key::Cint)
@@ -30,6 +41,7 @@ end
 
 function main()
   TrussC.setSetupFn(@cfunction(setup, Cvoid, ()))
+  TrussC.setDrawFn(@cfunction(draw, Cvoid, ()))
   TrussC.setKeyPressedFn(@cfunction(keyPressed, Cvoid, (Cint,)))
 
   # println(TrussC.greet())
